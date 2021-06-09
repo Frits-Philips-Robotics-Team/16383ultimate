207df8fe5ec0c36a6572c3a9bca57d23ab8c1669
Stijn van den Dungen <stijn@dupu.nl>
2021-06-09 15:36:22 +0200

    Cleaned and commented teleop class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/TeleOpFieldCentric.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/TeleOpFieldCentric.java
@@ -48,10 +48,20 @@ import org.firstinspires.ftc.teamcode.util.PersistentStorage;
 import java.util.Arrays;
 import java.util.List;
 
+/* This is the top-level class for the tele operated period, a large portion of robot control
+ * systems are implemented in multiple lower level classes. In this class all that is left to do is
+ * to initialize all the lower level things and assign actions to their gamepad buttons.
+ * This program is not perfect in that there are still a few more complicated functions being done
+ * here for example multiple nested if else statements which would be better suited for a separate class.
+ *
+ * We use an iterative op-mode structure for our tele-op since this nicely structures everything
+ * according to the phases in the program's execution. See the sa
+ */
 @Config
 @TeleOp(name="TeleOp Field Centric", group="basic")
 public class TeleOpFieldCentric extends OpMode
 {
+    // Declare op mode members and helping variables
     SampleMecanumDrive drive;
     RingHandling rings;
     GrabberHandling grabber;
@@ -60,6 +70,7 @@ public class TeleOpFieldCentric extends OpMode
     RevBlinkinLedDriver blinkin;
     RevBlinkinLedDriver.BlinkinPattern pattern;
     RevBlinkinLedDriver.BlinkinPattern prevPattern;
+
     ElapsedTime rotateTimer = new ElapsedTime();
     ElapsedTime matchTimer = new ElapsedTime();
 
@@ -75,26 +86,29 @@ public class TeleOpFieldCentric extends OpMode
     private int shootingStep;
     String target;
     Pose2d poseOffset;
+
     /*
      * Code to run ONCE when the driver hits INIT
      */
     @Override
     public void init() {
 
-        // Declare OpMode members.
+        // Initializing the robot classes has you pass through hardwaremap
         drive = new SampleMecanumDrive(hardwareMap);
         rings = new RingHandling(hardwareMap);
         grabber = new GrabberHandling(hardwareMap);
 
+        // Robot hardware initialized here uses this structure
         blocker = hardwareMap.get(Servo.class, "blocker");
         blocker.setPosition(0.75);
 
         blinkin = hardwareMap.get(RevBlinkinLedDriver.class, "blinkin");
         blinkin.setPattern(RevBlinkinLedDriver.BlinkinPattern.SKY_BLUE);
-        //drive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
+        // We use a stored value for our starting pose, either a default or from a previous opmode saving it
         drive.setPoseEstimate(PersistentStorage.currentPose);
 
+        // Giving certain helping variables a valid starting value
         wasRotating = false;
         rotationSetpoint = currentHeading = PersistentStorage.currentPose.getHeading();
         poseOffset = new Pose2d(0, 0, 0);
@@ -111,7 +125,7 @@ public class TeleOpFieldCentric extends OpMode
      */
     @Override
     public void init_loop() {
-        drive.update();
+        drive.update(); // We track our position whenever possible
     }
 
     /*
@@ -130,26 +144,28 @@ public class TeleOpFieldCentric extends OpMode
      */
     @Override
     public void loop() {
-        Pose2d poseEstimate = drive.getPoseEstimate();
+        Pose2d poseEstimate = drive.getPoseEstimate(); // Pose estimate is often called so we store it separately
 
         // Create a vector from gamepad x/y, then rotate it by current robot heading.
         // If applicable, use offset to change field centric orientation.
         Vector2d input = new Vector2d(gamepad1.left_stick_x, -gamepad1.left_stick_y).times(0.3 + 0.7 * gamepad1.right_trigger)
                 .rotated((-poseEstimate.getHeading()) - poseOffset.getHeading());
 
+        currentHeading = poseEstimate.getHeading();
+
+        // This block steers the robot towards a given rotation setpoint to counteract any drift
         if (Math.abs(rotationSetpoint - currentHeading) < Math.PI) {
             rotationError = rotationSetpoint - currentHeading;
-        }
-        else if (rotationSetpoint - currentHeading > 0){
+        } // We need multiple statements to deal with the rollover of the gyros range of -180 to 180 degrees
+        else if (rotationSetpoint - currentHeading > 0) {
             rotationError = (rotationSetpoint - currentHeading) - 2 * Math.PI;
         }
         else {
             rotationError = 2 * Math.PI + (rotationSetpoint - currentHeading);
         }
 
-        currentHeading = poseEstimate.getHeading();
-
-        if (gamepad1.right_stick_x == 0 && !wasRotating) {
+        // Does everything around when the driver decides to rotate the robot
+        if (gamepad1.right_stick_x == 0 && !wasRotating) { // Without any input we just do the little adjustments
             rotate = adjustmentSpeed * (rotationError);
             if (rotate >= 0) {
                 rotate = Math.pow(rotate, 0.6);
@@ -157,32 +173,35 @@ public class TeleOpFieldCentric extends OpMode
             else {
                 rotate = -Math.pow(-rotate, 0.6);
             }
-        } else if (wasRotating && gamepad1.right_stick_x == 0) {
+        } else if (wasRotating && gamepad1.right_stick_x == 0) { // When input has stopped since the last cycle
+            // We give a couple tenths of a second for the robot to settle before starting the adjustment
             if (rotateTimer.milliseconds() > 300) {
                 wasRotating = false;
             }
             rotationSetpoint = currentHeading;
             rotate = 0;
-        } else if (!wasRotating) {
+        } else if (!wasRotating) { // This happens when there is new input
             wasRotating = true;
             rotate = -gamepad1.right_stick_x * (0.3 + 0.7 * gamepad1.right_trigger);
             rotateTimer.reset();
-        } else {
+        } else { // For ongoing rotation input
             rotate = -gamepad1.right_stick_x * (0.3 + 0.7 * gamepad1.right_trigger);
             rotateTimer.reset();
         }
 
         if (Math.abs(rotate) < 0.05) {
-            rotate = 0;
+            rotate = 0; // Prevent the motors from receiving so little power they can't overcome static friction
         }
         // Pass rotated input + right stick value for rotation to drive function
         drive.setDrivePower(new Pose2d(input.getX(), input.getY(), rotate));
 
-        // Get ready for shooting and point at the goal
+        // Shooting the high goal automatically
         if (gamepad1.left_bumper) {
             if (rings.getRingNumber() != 0) {
                 pattern = RevBlinkinLedDriver.BlinkinPattern.HEARTBEAT_RED;
+
                 double calcHeading = rings.shootGetHeading(poseEstimate, target);
+                // Dealing with the gyro range of -180 to 180 is a pain
                 if (Math.abs(calcHeading - currentHeading) < Math.PI) {
                     drive.turn(calcHeading - currentHeading);
                 }
@@ -203,27 +222,27 @@ public class TeleOpFieldCentric extends OpMode
             blocker.setPosition(0.5);
         }
 
+        // Shooting three powershots automatically
         if (gamepad1.left_trigger == 1) {
-            Pose2d shootPose = new Pose2d(-2, -25, 0);
-            List<String> targetsList = Arrays.asList("red right", "red mid", "red left");
-
-            Vector2d headingVector = new Vector2d(drive.getPoseEstimate().minus(shootPose).getX(), drive.getPoseEstimate().minus(shootPose).getY());
-            Trajectory shootPower = drive.trajectoryBuilder(drive.getPoseEstimate(), headingVector.angle())
+            Pose2d shootPose = new Pose2d(-2, -25, 0); // Where the robot should shoot from
+            // Get the robot in position
+            Vector2d headingVector = new Vector2d(poseEstimate.minus(shootPose).getX(), poseEstimate.minus(shootPose).getY());
+            Trajectory shootPower = drive.trajectoryBuilder(poseEstimate, headingVector.angle())
                     .splineToSplineHeading(shootPose, headingVector.angle())
                     .build();
             drive.followTrajectory(shootPower);
             wasRotating = true;
-
+            // Signal to the other block that we wish to shoot
             shootingStep = 0;
         }
-
-        if (shootingStep != -1) {
+        if (shootingStep != -1) { // -1 Means we aren't shooting, anything else makes this trigger
             List<String> targetsList = Arrays.asList("red right", "red mid", "red left");
+            // This is called a state machine, this one is a bit weird since multiple cases have the same state
             switch (shootingStep) {
-                case 0: case 2: case 4:
-                    double calcHeading = rings.shootGetHeading(drive.getPoseEstimate(), targetsList.get(shootingStep/2));
-                    double currentHeading = drive.getPoseEstimate().getHeading();
-
+                case 0: case 2: case 4: // Here we start the shooting process
+                    double calcHeading = rings.shootGetHeading(poseEstimate, targetsList.get(shootingStep/2));
+                    double currentHeading = poseEstimate.getHeading();
+                    // Aiming
                     if (Math.abs(calcHeading - currentHeading) < Math.PI) {
                         drive.turn(calcHeading - currentHeading);
                     }
@@ -234,18 +253,18 @@ public class TeleOpFieldCentric extends OpMode
                         drive.turn(2 * Math.PI + (calcHeading - currentHeading));
                     }
                     wasRotating = true;
-
+                    // Shooting
                     target = targetsList.get(shootingStep/2);
                     rings.shoot();
-
+                    // Moving to the wait state
                     shootingStep++;
                     break;
-                case 1: case 3: case 5:
+                case 1: case 3: case 5: // Here we wait until the ring has been shot and then move to the next step
                     if (rings.state_s == RingHandling.shooterStates.NOTHING) {
                         shootingStep++;
                     }
                     break;
-                case 6:
+                case 6: // When all rings are done we reset
                     shootingStep = -1;
                     target = "red high";
                     break;
@@ -253,17 +272,19 @@ public class TeleOpFieldCentric extends OpMode
 
         }
 
+        // An unreliable way to take the blocker away when the shooting has stopped
         if (rings.state_s == RingHandling.shooterStates.NOTHING && pattern == RevBlinkinLedDriver.BlinkinPattern.HEARTBEAT_RED) {
             pattern = RevBlinkinLedDriver.BlinkinPattern.CP2_HEARTBEAT_SLOW;
             blocker.setPosition(0.5);
         }
 
-        if (gamepad2.right_bumper) {
+        if (gamepad2.right_bumper) { // Resetting the tracked position is on gamepad 2 to avoid accidents
             drive.setPoseEstimate(new Pose2d(-62, -63.8, Math.PI));
             PersistentStorage.currentPose = new Pose2d(-62, -63.8, Math.PI);
             wasRotating = true;
         }
 
+        // Intake control
         if (gamepad1.a) {
             rings.setIntake(1);
             pattern = RevBlinkinLedDriver.BlinkinPattern.CP2_HEARTBEAT_FAST;
@@ -276,6 +297,7 @@ public class TeleOpFieldCentric extends OpMode
             rings.setIntake(-1);
         }
 
+        // Grabber arm and gripper control
         if (gamepad1.dpad_down) {
             grabber.moveGrabber("down", "");
         }
@@ -285,7 +307,6 @@ public class TeleOpFieldCentric extends OpMode
         else if (gamepad1.dpad_up) {
             grabber.moveGrabber("inSize", "closed");
         }
-
         if (gamepad1.dpad_left) {
             grabber.moveGrabber("", "closed");
             grabberClosed = true;
@@ -295,6 +316,7 @@ public class TeleOpFieldCentric extends OpMode
             grabberClosed = false;
         }
 
+        // Choosing different targets to shoot at
         if (gamepad1.right_stick_y == -1) { // up on joystick
             target = "red high";
         }
@@ -308,7 +330,7 @@ public class TeleOpFieldCentric extends OpMode
             target = "red right";
         }
 
-        if (pattern != prevPattern) {
+        if (pattern != prevPattern) { // Set the blinkin led pattern when it has changed
             blinkin.setPattern(pattern);
             prevPattern = pattern;
         }
@@ -328,6 +350,7 @@ public class TeleOpFieldCentric extends OpMode
      */
     @Override
     public void stop() {
+        // When the op-mode is stopped we store the position for the next op-mode start
         PersistentStorage.currentPose = drive.getPoseEstimate();
     }
 
